4e5ab4e8d6b7ae488d94a12262a7b6aa538d4bb4
razaq009 <razaqbinfadhil@gmail.com>
2023-03-21 11:32:40 +0700

    boleehhh

---
+++ app/Views/admin/report.php
@@ -0,0 +1,49 @@
+<?= $this->extend('layout/template'); ?>
+
+<?= $this->section('content'); ?>
+
+<div class="home-content">
+    <?php if (session()->getFlashdata('pesan')) : ?>
+        <div class="alert alert-success" role="alert">
+            <?= session()->getFlashdata('pesan'); ?>
+        </div>
+    <?php endif ?>
+    <?php if (session()->getFlashdata('gagal')) : ?>
+        <div class="alert alert-danger" role="alert">
+            <?= session()->getFlashdata('gagal'); ?>
+        </div>
+    <?php endif ?>
+    <table class="table caption-top">
+        <caption>List of users</caption>
+        <thead>
+            <tr>
+                <th scope="col">#</th>
+                <th scope="col">First</th>
+                <th scope="col">Last</th>
+                <th scope="col">Handle</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <th scope="row">1</th>
+                <td>Mark</td>
+                <td>Otto</td>
+                <td>@mdo</td>
+            </tr>
+            <tr>
+                <th scope="row">2</th>
+                <td>Jacob</td>
+                <td>Thornton</td>
+                <td>@fat</td>
+            </tr>
+            <tr>
+                <th scope="row">3</th>
+                <td>Larry</td>
+                <td>the Bird</td>
+                <td>@twitter</td>
+            </tr>
+        </tbody>
+    </table>
+</div>
+
+<?= $this->endSection(); ?>
